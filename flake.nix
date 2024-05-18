5448c4c9127a4a45e1fa6f36f9c8057090e18849
Samuel Durante <samueldurante.h@gmail.com>
2024-05-18 21:54:38 +0000

    nix setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Toy programming language";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs.haskellPackages; [
+            stack
+            ghc
+            haskell-language-server
+          ];
+        };
+      });
+    };
+}
